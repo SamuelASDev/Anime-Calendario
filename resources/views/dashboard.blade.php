1241b21d14f414d7c6de8ea2403e89748be95cf7
SadFrogBR <samueltonis10@gmail.com>
2026-04-07 13:59:01 -0300

    Tela todos os animes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -253,7 +253,7 @@
 
                     <a href="{{ route('personal.animes.index') }}"
                         class="bg-purple-600 hover:bg-purple-700 text-white px-4 py-2 rounded-lg text-sm transition">
-                        ⚙️ Gerenciar animes
+                        ⚙️ Gerenciar Meus Animes
                     </a>
                 </div>
             </div>
